22c6f8a04be2b059d6b1cd58d1b75830ed71818c
lugengar <luciano.barbinii@gmail.com>
2024-06-09 23:52:11 -0300

    a

--- index.php
+++ index.php
@@ -13,18 +13,38 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio de Sesión</title>
-    <link rel="stylesheet" href="estiloscss/login.css">
+   
+    <link rel="stylesheet" href="estiloscss/styles.css">
+    <link rel="stylesheet" href="estiloscss/imagenes.css">
 </head>
 <body>
-    <form class="contenedor-login" action="codigophp/iniciosesion.php" method="post">
-        <img src="logo.jpg" alt="Logo" class="logo">
-        <img src="fotousuario.jpg" alt="Foto de Usuario" class="foto-usuario">
+    <div id="pagina3">
+        <div id="header">
+            <a class="logo imagen" ></a>
+            <div></div>
+        
+        </div>
+        <div id="subheader2" class="usuariohd imagen" style="background-size:20vh;background-position:bottom; box-shadow:none;"> 
+        </div>
+        <div id="contenido" style="background-color:transparent; box-shadow:none;">
+        <div class="contenido2">
+                <div class="con3" id="inicio">
+                    <h1 style="color:white;">Iniciar sesión</h1>
+                    <div class="scroll-y" style="height: 100%; width:40vh; padding-top: 2vh;">
+                        <form class="conscroll-y" method="post"  action="codigophp/iniciosesion.php" method="post">
+                            <input type="text" class="signomas boton imagen" name="username" id="username" required placeHolder="Nombre">
+                            <input type="password" class="signomas boton imagen" name="password" id="password" required  placeHolder="Contraseña">
+                            <input type="submit" class="avion boton imagen borde" value="Iniciar sesión">    
+                        </form>
+                        
+                    </div>
+                </div>
+            </div>
+        
+        </div>
 
-        <input type="text" name="username" id="username" required><br>
-     
-        <input type="password" class="boton-iniciar" name="password" id="password" required><br>
-        <input type="submit" value="Iniciar sesión">
-        <button class="boton-olvidado">Me olvidé mi contraseña</button>
-    </form>
+    </div>
+   
 </body>
 </html>
+
